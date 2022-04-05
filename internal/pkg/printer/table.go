e47e561ede6423c389ea92fb0389301ab150b079
Carmendelope <carmen@napptive.com>
2022-04-05 18:54:55 +0200

    Add a method to print the operation env in deploy command

--- internal/pkg/printer/table.go
+++ internal/pkg/printer/table.go
@@ -94,7 +94,7 @@ func (tp *TablePrinter) PrintResultOrError(result interface{}, err error) error 
 func (tp *TablePrinter) PrintResultOrErrorWithExtendedHeader(result interface{}, opEnv string, err error) error {
 
 	if err == nil && opEnv != "" {
-		fmt.Println(fmt.Sprintf("Operation environment: %s", opEnv))
+		fmt.Println(fmt.Sprintf("Operating environment: %s", opEnv))
 	}
 	return PrintResultOrError(tp, result, err)
 }
